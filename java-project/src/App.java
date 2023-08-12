20cc3b2ca1dc0f51bfaa2ccff0a81beed1430cfe
Andre Pereira <a204064@dac.unicamp.br>
2023-08-12 19:02:15 -0300

    Implementing object instantiation

--- java-project/src/App.java
+++ java-project/src/App.java
@@ -1,5 +1,48 @@
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import persons.Patron;
+import action.Subscription;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        LocalDate time_now = LocalDate.now();
+        Patron Ana = new Patron(        
+            "CPF", 
+            "463.781.195-5", 
+            "(11) 99856-5215", 
+            "algo@algo.com", 
+            "abc123", 
+            "Ana", 
+            "Samuel", 
+            'F', 
+            "Studant", 
+            0,
+            null, 
+            0,
+            null, 
+            time_now, 
+            false, 
+            null, 
+            null
+        );
+        
+        Subscription sp = new Subscription(
+            "Plus",
+            time_now
+        );
+
+
+        Ana.set_subscription(sp);
+
+        System.out.println(Ana.get_subscription().get_fee());
+
     }
+
+
+
+
+
+
+
+
 }
